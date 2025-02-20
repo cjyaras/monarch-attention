ab08addebc1a47d1e9170fbb5d5c5468037a8a97
Alec Xu <alecxu@Alecs-MacBook-Air-2.local>
2025-02-19 21:29:27 -0500

    forked huggingface dit

---
+++ dit/hooks/__init__.py
@@ -0,0 +1,12 @@
+"""
+Forked from https://github.com/huggingface/diffusers/blob/main/src/diffusers/hooks/__init__.py
+"""
+
+from diffusers.utils import is_torch_available
+
+
+if is_torch_available():
+    from .group_offloading import apply_group_offloading
+    from .hooks import HookRegistry, ModelHook
+    from .layerwise_casting import apply_layerwise_casting, apply_layerwise_casting_hook
+    from .pyramid_attention_broadcast import PyramidAttentionBroadcastConfig, apply_pyramid_attention_broadcast
